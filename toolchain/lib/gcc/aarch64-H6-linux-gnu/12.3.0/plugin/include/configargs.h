14dd498e99ee218e179522224195f340685cb25a
shen <664376944@qq.com>
2024-06-07 17:57:20 +0800

    增加交叉编译工具链

---
+++ toolchain/lib/gcc/aarch64-H6-linux-gnu/12.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/shen/git_rep/buildroot-2024.02.2/output/host --sysconfdir=/home/shen/git_rep/buildroot-2024.02.2/output/host/etc --enable-static --target=aarch64-H6-linux-gnu --with-sysroot=/home/shen/git_rep/buildroot-2024.02.2/output/host/aarch64-H6-linux-gnu/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --enable-plugins --enable-lto --with-gmp=/home/shen/git_rep/buildroot-2024.02.2/output/host --with-mpc=/home/shen/git_rep/buildroot-2024.02.2/output/host --with-mpfr=/home/shen/git_rep/buildroot-2024.02.2/output/host --with-pkgversion='Buildroot 2024.02.2' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-abi=lp64 --with-cpu=cortex-a53 --enable-languages=c,c++ --with-build-time-tools=/home/shen/git_rep/buildroot-2024.02.2/output/host/aarch64-H6-linux-gnu/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a53" } };
